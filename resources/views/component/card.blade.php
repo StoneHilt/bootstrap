0f81d0a8d711275342622956fb7d287130980abf
ahgreen <anthony.green@avastechnology.com>
2025-01-17 11:08:04 -0500

    Card title, subtitle and text blocks should support any html tag, not just the default ones.

--- resources/views/component/card.blade.php
+++ resources/views/component/card.blade.php
@@ -5,9 +5,9 @@
         <img src="{{ $headerImage }}" class="card-img-top" alt="{{ $headerImage ?? 'Image Top' }}">
     @endif
     <div class="card-body">
-        @if($title->isNotEmpty())<h5 {{ $title->attributes->class(['card-title']) }}>{{ $title }}</h5>@endif
-        @if($subtitle->isNotEmpty())<h6 {{ $subtitle->attributes->class(['card-subtitle mb-2 text-muted']) }}>{{ $subtitle }}</h6>@endif
-        @if($text->isNotEmpty())<p {{ $text->attributes->class(['card-text']) }}>{{ $text }}</p>@endif
+        @if($title->isNotEmpty())<{{ $title->attributes->get('tag', 'h5') }} {{ $title->attributes->class(['card-title'])->except(['tag']) }}>{{ $title }}</{{ $title->attributes->get('tag', 'h5') }}>@endif
+        @if($subtitle->isNotEmpty())<{{ $subtitle->attributes->get('tag', 'h6') }} {{ $subtitle->attributes->class(['card-subtitle mb-2 text-muted'])->except(['tag']) }}>{{ $subtitle }}</{{ $subtitle->attributes->get('tag', 'h6') }}>@endif
+        @if($text->isNotEmpty())<{{ $text->attributes->get('tag', 'p') }} {{ $text->attributes->class(['card-text'])->except(['tag']) }}>{{ $text }}</{{ $text->attributes->get('tag', 'p') }}>@endif
         @if($slot->isNotEmpty())
             {{ $slot }}
         @endif
